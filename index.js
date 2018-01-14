5f4a69c40ebc6e3d28a79fe65c6d43d76b503d27
Lisa Lee <snickers495@gmail.com>
2018-01-14 17:56:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,10 @@
    this.doCharity = function(){
      return "I like to help people."
    };
+   this.releasePressStatement = function(){
+     return "You will see great things from Scuber.";
+   };
+   this.sayHi = function(){
+     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+   }
  }
